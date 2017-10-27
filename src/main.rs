68c4bfb297ce1f9dd271019bb68148768512506f
Dan Murphy <danwright91@gmail.com>
2017-10-28 00:05:27 +0100

    Successfully parsing SANs from a PEM certificate

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,24 @@ fn main() {
     let input_file_path = args.value_of("InputFile").unwrap();
     let cert_bytes = get_cert_bytes(input_file_path).unwrap();
     let cert = X509::from_pem(&cert_bytes).unwrap();
+    
+    println!("------------COMMON NAMES------------");
     let cert_common_names = get_subject_name(&cert);
     for cn in cert_common_names {
         match cn {
             Ok(cn) => println!("{:?}", cn),
             Err(err) => println!("{:?}", err)
+        }
+    }
+
+    println!("------------SUBJECT ALT NAMES------------");
+    let cert_subject_alt_names = get_subject_alt_names(&cert);
+    match cert_subject_alt_names {
+        None => println!("{:?}", "No SANs listed in certificate".to_string()),
+        Some(sans) => {
+            for san in sans {
+                println!("{:?}", san)
+            }
         }
     }
 }
@@ -44,4 +57,15 @@ fn get_subject_name(cert: &X509) -> Vec<Result<OpensslString, ErrorStack>> {
     let entries = cert.subject_name();
     let common_name_entries = entries.entries_by_nid(nid::COMMONNAME);
     common_name_entries.map(|x| (*x).data().as_utf8()).collect()
+}
+
+fn get_subject_alt_names(cert: & X509) -> Option<Vec<String>> {
+    let entries = cert.subject_alt_names();
+    match entries {
+        None => None,
+        Some(e) => Some(e.into_iter()
+            .filter_map(|x| x.dnsname().and_then(|x| Some(x.to_owned())))
+            .filter_map(Option::Some)
+            .collect())
+    }
 }

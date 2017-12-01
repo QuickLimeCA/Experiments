43e54fbc0dcab25287b44677e46dcf172e75fc22
Daniel Murphy <danhatesnumbers@gmail.com>
2017-12-01 15:40:36 +0000

    Added schema validation using Valico

--- schema/src/main.rs
+++ schema/src/main.rs
@@ -1,10 +1,16 @@
 extern crate clap;
+extern crate serde;
+extern crate serde_json;
+extern crate valico;
 
 use clap::{Arg, App};
+use serde_json::{Value, from_str};
+use valico::json_schema;
 
 use std::io;
 use std::io::Read;
 use std::fs::File;
+use std::path::Path;
 
 fn main() {
     let args = App::new("Schema Validator")
@@ -21,17 +27,32 @@ fn main() {
             .required(true))
         .get_matches();
 
-    let input_json_path = args.value_of("InputJson").unwrap();
+    let input_json_path = Path::new(args.value_of("InputJson").unwrap());
     let input_json_data = read_file(&input_json_path).unwrap();
 
-    let input_schema_path = args.value_of("InputSchema").unwrap();
+    let input_schema_path = Path::new(args.value_of("InputSchema").unwrap());
     let input_schema_data = read_file(&input_schema_path).unwrap();
 
-    println!("JSON: {:?}", input_json_data);
-    println!("Schema: {:?}", input_schema_data);
+    let schema: Value = from_str(&input_schema_data).unwrap();
+    let data: Value = from_str(&input_json_data).unwrap();
+    let mut scope = json_schema::Scope::new();
+
+    let schema_compiled = match scope.compile_and_return(schema.clone(), false) {
+        Ok(schema) => schema,
+        Err(err) => panic!("Error in schema {:?}", err)
+    };
+
+    let state = schema_compiled.validate(&data.as_object().unwrap().get("data").unwrap());
+
+    println!("Is JSON Valid? {:?}", state.is_valid());
+    if(state.is_valid() == false) {
+        for err in state.errors {
+            println!("{:?} {:?} {:?}", err.get_path(), err.get_title(), err.get_detail());
+        }
+    }
 }
 
-fn read_file(input_file_path: &str) -> Result<String, io::Error> {
+fn read_file(input_file_path: &Path) -> Result<String, io::Error> {
     let mut contents = String::new();
     File::open(input_file_path)?.read_to_string(&mut contents)?;
     Ok(contents)

4b805d1caef0be9b1f6f6e6f22bf5846d6d47aed
Daniel Murphy <danhatesnumbers@gmail.com>
2017-12-04 14:44:24 +0000

    Further example of handling unknown versions in the match statement for structver

--- structver/src/main.rs
+++ structver/src/main.rs
@@ -15,22 +15,31 @@ pub fn main() {
     let v2: Result<MyStruct, serde_json::Error> = serde_json::from_str(v2_data);
     let v3: Result<MyStruct, serde_json::Error> = serde_json::from_str(v3_data);
 
+    #[allow(unreachable_patterns)]
     match v1 {
         Ok(MyStruct::V1(data)) => println!("V1: {:?}", data.test_prop),
         Ok(MyStruct::V2(data)) => println!("V2: {:?}", data.test_prop),
-        Err(err) => println!("Couldn't parse: {:?}", err)
+        Ok(MyStruct::V3(data)) => println!("V3: {:?}", data.test_prop),
+        Err(err) => println!("Couldn't parse: {:?}", err),
+        _ => println!("Unknown version")
     }
 
+    #[allow(unreachable_patterns)]
     match v2 {
         Ok(MyStruct::V1(data)) => println!("V1: {:?}", data.test_prop),
         Ok(MyStruct::V2(data)) => println!("V2: {:?}", data.test_prop),
-        Err(err) => println!("Couldn't parse: {:?}", err)
+        Ok(MyStruct::V3(data)) => println!("V3: {:?}", data.test_prop),
+        Err(err) => println!("Couldn't parse: {:?}", err),
+        _ => println!("Unknown version")
     }
 
+    #[allow(unreachable_patterns)]
     match v3 {
         Ok(MyStruct::V1(data)) => println!("V1: {:?}", data.test_prop),
         Ok(MyStruct::V2(data)) => println!("V2: {:?}", data.test_prop),
-        Err(err) => println!("Couldn't parse: {:?}", err)
+        //Ok(MyStruct::V3(data)) => println!("V3: {:?}", data.test_prop),
+        Err(err) => println!("Couldn't parse: {:?}", err),
+        _ => println!("Unknown version")
     }
 }
 
@@ -47,8 +56,15 @@ pub struct MyStruct2 {
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct MyStruct3 {
+    pub test_prop: Vec<i64>
+}
+
+#[derive(Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum MyStruct {
     V1(MyStruct1),
-    V2(MyStruct2)
+    V2(MyStruct2),
+    V3(MyStruct3)
 }
